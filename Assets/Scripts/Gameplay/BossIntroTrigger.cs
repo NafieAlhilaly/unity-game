a0fa3d7b1cba409d03c0b945d6882b22faed03e5
Nafie <warrior259@gmail.com>
2023-08-05 15:03:30 +0300

    chore: replace hunter sprite and rename it

--- Assets/Scripts/Gameplay/BossIntroTrigger.cs
+++ Assets/Scripts/Gameplay/BossIntroTrigger.cs
@@ -7,16 +7,18 @@ public class SceneEndingController : MonoBehaviour
     [SerializeField] GameObject DarkSingularity;
     [SerializeField] GameObject Player;
     [SerializeField] bool isBossFightStarted = false;
+    [SerializeField] float DarkSingularitySpeed = 1f;
+    [SerializeField] float BossSpeed = 1f;
 
     void Update()
     {
         if (isBossFightStarted && Boss.transform.position.y <= BossEndPosition)
         {
-            Boss.transform.position += Vector3.up * Time.deltaTime;
+            Boss.transform.position += Vector3.up * Time.deltaTime * BossSpeed;
         }
         if (isBossFightStarted && Boss.transform.position.y >= BossEndPosition)
         {
-            DarkSingularity.transform.position = transform.position = Vector3.MoveTowards(DarkSingularity.transform.position, Player.transform.position, Time.deltaTime * 1);
+            DarkSingularity.transform.position = transform.position = Vector3.MoveTowards(DarkSingularity.transform.position, Player.transform.position, Time.deltaTime * DarkSingularitySpeed);
             DarkSingularity.transform.localScale += Vector3.one * Time.deltaTime / 2;
         }
     }

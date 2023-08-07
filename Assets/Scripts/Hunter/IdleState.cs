f8b4a5d7b8f257e93089ab2c5ce5351a82e5f8ec
Nafie <warrior259@gmail.com>
2023-08-07 10:45:50 +0300

    chore: add states for hunter character

---
+++ Assets/Scripts/Hunter/IdleState.cs
@@ -0,0 +1,12 @@
+namespace Hunter
+{
+    public class IdleState : BaseState
+    {
+
+        public override void StartState(StateManager stateManager){}
+        public override void UpdateState(StateManager stateManager){}
+        public override void ApplyPlayerEffect() { }
+
+
+    }
+}

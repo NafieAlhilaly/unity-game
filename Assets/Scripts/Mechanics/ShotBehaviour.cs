6e1bef459d82fa89c4257c6a7d2744a1a0c4fe68
Nafie <warrior259@gmail.com>
2023-08-11 02:20:46 +0300

    feat: add auto attack

--- Assets/Scripts/Mechanics/ShotBehaviour.cs
+++ Assets/Scripts/Mechanics/ShotBehaviour.cs
@@ -6,15 +6,30 @@ namespace Platformer.Mechanics
     {
         [SerializeField] float Speed = 10f;
         [SerializeField] AudioSource ShotSound;
+        public GameObject Player;
+        [SerializeField] bool TargetPlayer = false;
+        Vector3 CurrentPlayerPosition;
 
-        void Start()
+        void Awake()
         {
+            if (Player == null)
+            {
+                Player = GameObject.FindGameObjectWithTag("Player");
+            }
+            CurrentPlayerPosition = (Player.transform.position - transform.position).normalized;
             ShotSound.Play();
         }
         void Update()
         {
-            transform.position += Vector3.down * Time.deltaTime * Speed;
-            if(transform.position.y <= -20f)
+            if (TargetPlayer)
+            {
+                transform.position += Speed * Time.deltaTime * CurrentPlayerPosition;
+            }
+            else
+            {
+                transform.position += Speed * Time.deltaTime * Vector3.down;
+            }
+            if (transform.position.y <= -20f)
             {
                 Destroy(gameObject);
             }

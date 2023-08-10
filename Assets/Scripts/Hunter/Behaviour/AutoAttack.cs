013a6eae97934b9ce0f6c9f7abf832b8cd22c1f7
Nafie <warrior259@gmail.com>
2023-08-11 02:20:46 +0300

    feat: add auto attack

---
+++ Assets/Scripts/Hunter/Behaviour/AutoAttack.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using Platformer.Mechanics;
+
+namespace Hunter.Behaviour
+{
+    public class AutoAttack : MonoBehaviour
+    {
+        public float RotationSpeed = 0.3f;
+        private Quaternion LookRotation;
+        private Vector3 Direction;
+        [SerializeField] GameObject Player;
+        [SerializeField] float AngleModifier = 30f;
+        [SerializeField] Shot ElectricShot;
+        enum AttackState
+        {
+            Idle,
+            Charging,
+            Attacking
+        }
+        [SerializeField] AttackState CurrentAttackState = AttackState.Idle;
+
+        void Update()
+        {
+            RotateToTarget(Player.transform);
+            if (CurrentAttackState == AttackState.Idle)
+            {
+                CurrentAttackState = AttackState.Charging;
+                Invoke(nameof(InstantiateAttack), 6f);
+            }
+        }
+
+        public void RotateToTarget(Transform Target)
+        {
+            Direction = (Target.position - transform.position).normalized;
+            float angle = (Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg) + AngleModifier;
+            LookRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, LookRotation, Time.deltaTime * RotationSpeed);
+        }
+
+        void InstantiateAttack()
+        {
+            CurrentAttackState = AttackState.Attacking;
+            ElectricShot.Player = Player;
+            Shot s = Instantiate(ElectricShot, transform);
+            CurrentAttackState = AttackState.Idle;
+        }
+    }
+
+}
